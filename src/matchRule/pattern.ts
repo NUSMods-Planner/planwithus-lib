316148ae9037fb2bbc99b222f889718bbdfbb922
Ng Wei En <weien1292@gmail.com>
2021-05-29 11:28:13 +0800

    matchRule: Enforce pattern string format

--- src/matchRule/pattern.ts
+++ src/matchRule/pattern.ts
@@ -2,7 +2,10 @@ import { JSONSchemaType } from "ajv";
 
 type Pattern = string;
 
-const patternSchema: JSONSchemaType<Pattern> = { type: "string" };
+const patternSchema: JSONSchemaType<Pattern> = {
+  type: "string",
+  pattern: "^[A-Z0-9x*]*$",
+};
 
 export type { Pattern };
 export { patternSchema };
