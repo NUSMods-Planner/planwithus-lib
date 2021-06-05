45acdb10e17ac01456428c97849db1d0e8dbd9ce
Ng Wei En <weien1292@gmail.com>
2021-06-05 13:43:07 +0800

    Refactor block loading logic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import Ajv from "ajv";
+import fs from "fs/promises";
+import glob from "globby";
 import yaml from "js-yaml";
+import path from "path";
 
 import type { Block } from "./block";
 import { blockSchema } from "./block";
 import { matchRuleSchema } from "./matchRule";
 import { satisfyRuleSchema } from "./satisfyRule";
+import { Verifier } from "./verifier";
+
+const BLOCK_CLASSES = ["primary", "second", "minor"];
+const PATH_PREFIX = path.join(__dirname, "../blocks");
 
 const ajv = new Ajv({
   allowUnionTypes: true,
@@ -23,4 +30,27 @@ const parseYAML = (contents: string): Block => {
   return block;
 };
 
-export { parseYAML };
+const loadBlocks = async (type: string): Promise<Verifier> => {
+  const files = await glob(`${PATH_PREFIX}/${type}/**/*.yml`);
+  const fileContents = await Promise.all(
+    files.map((fileName) => fs.readFile(fileName, "utf8"))
+  );
+
+  const verifier = new Verifier();
+  files.forEach((filename, i) =>
+    verifier.addBlock(
+      path.basename(filename).replace(/\.yml$/, ""),
+      parseYAML(fileContents[i])
+    )
+  );
+  return verifier;
+};
+
+const initVerifiers = async (): Promise<Record<string, Verifier>> => {
+  const verifiers = await Promise.all(BLOCK_CLASSES.map(loadBlocks));
+  return Object.fromEntries(
+    BLOCK_CLASSES.map((prefix, i) => [prefix, verifiers[i]])
+  );
+};
+
+export { BLOCK_CLASSES, initVerifiers };
--- src/test.ts
+++ src/test.ts
@@ -1,39 +1,17 @@
-import fs from "fs/promises";
-import path from "path";
-import glob from "globby";
-
+import { initVerifiers } from ".";
 import type { MatchRule } from "./matchRule";
-import { parseYAML } from "./";
-import { Verifier } from "./verifier";
-
-const PATH_PREFIX = path.join(__dirname, "../blocks");
-
-const loadBlocks = async (type: string): Promise<Verifier> => {
-  const files = await glob(`${PATH_PREFIX}/${type}/**/*.yml`);
-  const fileContents = await Promise.all(
-    files.map((fileName) => fs.readFile(fileName, "utf8"))
-  );
-
-  const verifier = new Verifier();
-  files.forEach((filename, i) => {
-    console.log(filename);
-    verifier.addBlock(
-      path.basename(filename).replace(/\.yml$/, ""),
-      parseYAML(fileContents[i])
-    );
-  });
-  return verifier;
-};
 
 const main = async () => {
-  const primaryVerifier = await loadBlocks("primary");
-  console.log(JSON.stringify(primaryVerifier.blocks, null, 2));
+  const { primary, second, minor } = await initVerifiers();
+
+  console.log(JSON.stringify(primary.blocks, null, 2));
   console.log("---");
-  const secondVerifier = await loadBlocks("second");
-  console.log(JSON.stringify(secondVerifier.blocks, null, 2));
+  console.log(JSON.stringify(second.blocks, null, 2));
   console.log("---");
-  const matchRules = primaryVerifier.find("cs-hons-2020/found")
-    .match as MatchRule[];
+  console.log(JSON.stringify(minor.blocks, null, 2));
+  console.log("---");
+
+  const matchRules = primary.find("cs-hons-2020/found").match as MatchRule[];
   console.log(matchRules);
   if (typeof matchRules[0] === "object" && "and" in matchRules[0]) {
     const andRules = matchRules[0].and as MatchRule[];
