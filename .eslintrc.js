f0c74dc0ea520db0ff36bad4040f8b841d4d9f3e
Ng Wei En <weien1292@gmail.com>
2021-06-05 10:58:04 +0800

    pattern: Add test suite for validation

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
     ecmaVersion: 12,
   },
   plugins: ["@typescript-eslint"],
-  rules: {},
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
 };
--- src/matchRule/pattern.ts
+++ src/matchRule/pattern.ts
@@ -4,7 +4,9 @@ type Pattern = string;
 
 const patternSchema: JSONSchemaType<Pattern> = {
   type: "string",
-  pattern: "^[A-Z0-9x*]*$",
+  pattern: "^[A-Z0-9x*]+$",
+  errorMessage:
+    "pattern should be a non-empty string composed of A-Z, 0-9, x or *",
 };
 
 export type { Pattern };
