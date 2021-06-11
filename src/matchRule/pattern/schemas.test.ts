7b80d0a05449faa159a1f595c1673d8ebe8f9cbf
Ng Wei En <weien1292@gmail.com>
2021-06-11 16:05:25 +0800

    pattern: Refine test suite

--- src/matchRule/pattern/schemas.test.ts
+++ src/matchRule/pattern/schemas.test.ts
@@ -1,8 +1,12 @@
-import { should } from "chai";
+import chai from "chai";
+import chaiSubset from "chai-subset";
 import { assert, constantFrom, property, stringOf, tuple } from "fast-check";
 
 import { ajv } from "../../index.test";
 import { patternSchema } from "./schemas";
+
+chai.use(chaiSubset);
+chai.should();
 
 const ajvValidate = ajv.compile(patternSchema);
 
@@ -25,34 +29,26 @@ const pattern = tuple(modulePrefix, moduleNumber, moduleSuffix).map(
   (t: [string, string, string]) => t.join("")
 );
 
-should();
-
-const isValidPattern = (patternStr: string) =>
-  ajvValidate(patternStr).should.be.true;
-
 const isInvalidPattern = (patternStr: string) => {
   ajvValidate(patternStr).should.be.false;
   ajvValidate.should.have.property("errors");
 
   const errors = ajvValidate.errors!;
   errors.should.be.an("array");
-  errors.should.have.lengthOf.above(0);
-
-  const error = errors[0];
-  error.should.have.property("message");
-  error.message!.should.equal(
-    "pattern should be a non-empty string composed of A-Z, 0-9, x or *"
-  );
+  errors.should.containSubset([
+    {
+      instancePath: "",
+      message:
+        "pattern should be a non-empty string composed of A-Z, 0-9, x or *",
+    },
+  ]);
 };
 
 describe("patternSchema", () => {
-  it("should validate valid module code", () =>
-    ["ACC1002", "GER1000", "IS1103", "CS1231S", "MA1102R", "MA2101S"].forEach(
-      isValidPattern
-    ));
-  it("should validate wildcard", () => isValidPattern("*"));
   it("should validate non-empty pattern composed of A-Z, 0-9, x or *", () =>
-    assert(property(pattern, (patternStr) => ajvValidate(patternStr))));
+    assert(property(pattern, ajvValidate), {
+      examples: [["*"], ["ACC1002"], ["IS1103"], ["CS1231S"], ["MA1102R"]],
+    }));
 
   it("should not validate empty pattern", () => isInvalidPattern(""));
   it("should not validate pattern with invalid characters", () =>
