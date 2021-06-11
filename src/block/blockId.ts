b04b0c7632f9ca04bcc3900d03b6029dd4e3b885
Ng Wei En <weien1292@gmail.com>
2021-06-11 16:38:07 +0800

    blockId: Refactor into types + schemas

--- src/block/blockId.ts
+++
@@ -1,7 +0,0 @@
-import { JSONSchemaType } from "ajv";
-
-type BlockId = string;
-const blockIdSchema: JSONSchemaType<BlockId> = { type: "string" };
-
-export type { BlockId };
-export { blockIdSchema };
