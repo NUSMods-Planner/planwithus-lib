f13bcde252968c9feaa9ab215a8a77c02ef8acaa
Ng Wei En <weien1292@gmail.com>
2021-12-11 18:22:54 +0800

    Upgrade ajv to 8.8.2
    This commit also disables some checks in `Some`-related files, which
    require the `type` property to be explicitly specified.

--- src/some/schemas.ts
+++ src/some/schemas.ts
@@ -6,14 +6,16 @@ const someSchema = <T>(
   typeSchema: JSONSchemaType<T>,
   schema: JSONSchemaType<T>,
   arrayProperties: Record<string, unknown>
-): JSONSchemaType<Some<T>> => ({
-  type: [typeSchema.type, "array"].flat(),
-  if: { type: typeSchema.type },
-  then: schema,
-  else: {
-    if: { type: "array" },
-    then: { ...arrayProperties, type: "array", items: schema },
-  },
-});
+): JSONSchemaType<Some<T>> =>
+  ({
+    type: [typeSchema.type, "array"].flat(),
+    if: { type: typeSchema.type },
+    then: schema,
+    else: {
+      if: { type: "array" },
+      then: { ...arrayProperties, type: "array", items: schema },
+    },
+  } as JSONSchemaType<Some<T>>); // this disables some checks which require
+// typeSchema.type to be explicitly specified
 
 export { someSchema };
--- src/some/typeSchemas.ts
+++ src/some/typeSchemas.ts
@@ -4,9 +4,11 @@ import type { Some } from "./types";
 
 const someTypeSchema = <T>(
   typeSchema: JSONSchemaType<T>
-): JSONSchemaType<Some<T>> => ({
-  type: [typeSchema.type, "array"].flat(),
-  anyOf: [typeSchema, { type: "array", items: typeSchema }],
-});
+): JSONSchemaType<Some<T>> =>
+  ({
+    type: [typeSchema.type, "array"].flat(),
+    anyOf: [typeSchema, { type: "array", items: typeSchema }],
+  } as JSONSchemaType<Some<T>>); // this disables some checks which require
+// typeSchema.type to be explicitly specified
 
 export { someTypeSchema };
